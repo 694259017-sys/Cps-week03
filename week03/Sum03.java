9fb8d6ad54e25cf631681f225e3992c25a0da318
694259017-sys <694259017@webmail.npru.ac.th>
2026-07-02 15:22:14 +0700

    Sum03 Remove word Sum of

--- week03/Sum03.java
+++ week03/Sum03.java
@@ -4,5 +4,5 @@ int num1 = Integer.parseInt(IO.readln());
 IO.print("Enter Your Second Number :");
 int num2 = Integer.parseInt(IO.readln());
 int sum = num1+num2;
-IO.println("Sum of " + num1 + " and " + num2 + " = " + sum);
+IO.println(num1 + " and " + num2 + " = " + sum);
 }
